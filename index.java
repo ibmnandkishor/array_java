9005bdb7b434d86c929cd78d689ac5c4d3a8d109
Nand kishor <121294572+ibmnandkishor@users.noreply.github.com>
2024-07-24 10:12:41 +0530

    Add files via upload

---
+++ index.java
@@ -0,0 +1,36 @@
+// class first{
+//     public static void main(String args[]){
+//         String cars[]={"Volvo", "BMW", "Ford", "Mazda"};
+//         for(int i=0;i<cars.length;i++){
+//             System.out.println(cars[i]);
+//         }
+//     }
+// }
+
+// class second{
+//     public static void main(String args[]){
+//         String[] car={"Volvo", "BMW", "Ford", "Mazda"};
+//         for(String i:car){
+//             System.out.println(i);
+//         }
+//     }
+// } 
+
+
+
+// class first{
+//     public static void main(String args[]){
+// String txt = "Hello Everybody";
+// System.out.println(txt.indexOf("e"));
+//     }
+// }
+
+
+class first{
+    public static void main(String args[]){
+     var name = "nandkishor";
+     var age = 21;
+     var city="hyderabad";
+     System.out.println(name+" "+age+" "+city);
+        }
+    }
